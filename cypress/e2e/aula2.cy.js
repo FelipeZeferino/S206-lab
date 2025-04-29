7c78434086a04ac675edba448ed646ab937557d2
FelipeZeferino <fzeferino16@gmail.com>
2025-04-29 00:18:04 -0300

    first commit

---
+++ cypress/e2e/aula2.cy.js
@@ -0,0 +1,67 @@
+///<reference = cypress>
+
+describe('User Auth', () => {
+  it('User creation success', () => {
+      cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login');
+      cy.get('.btn-link').click();
+      cy.get('#firstName').type('Felipe');
+      cy.get('#Text1').type('Zeferino');
+      cy.get('#username').type('felipemsz');
+      cy.get('#password').type('teste123');
+      cy.get('.btn-primary').click();
+      cy.get('.ng-binding').should('contain.text', 'Registration successful');
+  })
+  it('User creation fail', () => {
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login');
+    cy.get('.btn-link').click();
+    cy.get('#firstName').type('Felipe');
+    cy.get('#Text1').type('Zeferino');
+    cy.get('#username').type('felipemsz');
+    cy.get('.btn-primary').should('be.disabled');
+    // cy.get('.ng-binding').should('contain.text', 'Registration successful');
+})
+it('login success', () => {
+  let { id, password } = createUser();
+
+  cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login');
+  cy.get('#username').type(id);
+  cy.get('#password').type(password);
+  cy.get('.btn-primary').click();
+  cy.get('h1.ng-binding').should('contain.text', id);
+})
+it('login and delete user and login again fail', () => {
+  let { id, password } = createUser();
+
+  cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login');
+  cy.get('#username').type(id);
+  cy.get('#password').type(password);
+  cy.get('.btn-primary').click();
+  cy.get('h1.ng-binding').should('contain.text', id);
+  cy.get('.ng-binding > a').click();
+  cy.get('.btn').click();
+  cy.get('#username').type(id);
+  cy.get('#password').type(password);
+  cy.get('.btn-primary').click();
+  cy.get('.ng-binding').should('contain.text', 'Username or password is incorrect')
+})
+
+})
+
+function createUser() {
+  let hour = new Date().getHours().toString();
+  let minutes = new Date().getMinutes().toString();
+  let seconds = new Date().getSeconds().toString();
+  let id = hour + minutes + seconds + 'id';
+  let password = hour + minutes + seconds + 'senha';
+  let infos = [id, password];
+
+  cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login');
+  cy.get('.btn-link').click();
+  cy.get('#firstName').type(id);
+  cy.get('#Text1').type(id);
+  cy.get('#username').type(id);
+  cy.get('#password').type(password);
+  cy.get('.btn-primary').click();
+  cy.get('.ng-binding').should('contain.text', 'Registration successful');
+  return { id, password };
+}
